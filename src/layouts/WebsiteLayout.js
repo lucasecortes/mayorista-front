f42b398d39c1693022efd0fdeda88f9cb074a768
lec89 <lucascorts@hotmail.com>
2023-03-09 14:30:40 -0300

    WebsiteLayout added

---
+++ src/layouts/WebsiteLayout.js
@@ -0,0 +1,11 @@
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+
+export default function WebsiteLayout() {
+  return (
+    <div>
+        <Header />
+        <Footer />
+    </div>
+  )
+}
