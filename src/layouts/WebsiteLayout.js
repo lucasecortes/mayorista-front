6310ddff71cbfdea7bf3989e753b8b9e736660b1
lec89 <lucascorts@hotmail.com>
2023-03-13 11:37:12 -0300

    underconstruction page added

--- src/layouts/WebsiteLayout.js
+++ src/layouts/WebsiteLayout.js
@@ -1,10 +1,12 @@
 import Footer from '../components/Footer'
 import Header from '../components/Header'
+import UnderConstruction from '../pages/UnderConstruction'
 
 export default function WebsiteLayout() {
   return (
     <div>
         <Header />
+        <UnderConstruction />
         <Footer />
     </div>
   )
